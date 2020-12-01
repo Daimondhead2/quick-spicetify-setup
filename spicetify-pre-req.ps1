74eea5b68dfd5e60775dfeff906d6e9e3221103f
Daimondhead2 <maanmeher@gmail.com>
2020-12-01 23:17:35 +0000

    Update spicetify-pre-req.ps1
    typo

--- spicetify-pre-req.ps1
+++ spicetify-pre-req.ps1
@@ -1,7 +1,7 @@
-Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+Set-ExecutionPolicy Unrestricted -scope CurrentUser
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y git
-scoop bucket add nerd-font
+scoop bucket add nerd-fonts
 scoop install Open-Sans Raleway --global
 scoop install Ubuntu-NF --global
