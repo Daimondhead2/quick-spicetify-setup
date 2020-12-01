0d87cc59738126a9d7940cc42653d7788b0961ae
Daimondhead2 <maanmeher@gmail.com>
2020-12-01 12:21:07 +0000

    Delete spicetify-pre-req.ps1

--- spicetify-pre-req.ps1
+++
@@ -1,8 +0,0 @@
-Set-ExecutionPolicy RemoteSigned -scope CurrentUser
-Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install -y git
-Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.ps1" | Invoke-Expression
-scoop bucket add nerd-font
-scoop install Open-Sans Raleway --global
-scoop install Ubuntu-NF --global
